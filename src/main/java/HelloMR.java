b9411ad9c514596981f4864a677678f05ed744b8
Ophir Cohen <ophchu@gmail.com>
2012-12-06 13:44:07 +0200

    Add first pom and structure

---
+++ src/main/java/HelloMR.java
@@ -0,0 +1,19 @@
+/*
+* LivePerson copyrights will be here...
+*/
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ophirc
+ * @version 1.0.0
+ * @since 12/6/12, 13:40
+ */
+public class HelloMR {
+  private static final Logger LOG = LoggerFactory.getLogger(HelloMR.class);
+
+  public static void main(String[] args) {
+    System.out.println("Hello MR!");
+  }
+}
