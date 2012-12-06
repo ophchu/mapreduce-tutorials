9838de88904d08288b477390ce3f5ea446fb8592
Ophir Cohen <ophchu@gmail.com>
2012-12-06 13:47:35 +0200

    Add Hello World

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+* LivePerson copyrights will be here...
+*/
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ophirc
+ * @version 1.0.0
+ * @since 12/6/12, 13:46
+ */
+public class HelloWorld {
+  private static final Logger LOG = LoggerFactory.getLogger(HelloWorld.class);
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
