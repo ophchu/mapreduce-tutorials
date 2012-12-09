2704959a23c3df804020bf692cf6a87972651b07
Ophir Cohen <ophchu@gmail.com>
2012-12-09 14:07:54 +0200

    1. Add bash running script

---
+++ resources/cli/word_count.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+########################## Run wordcount example
+hadoop jar /path/to/my/jar simple.wordcount.WordCountJob /home/ophirc/mr_examples/wordcount/simple/in /home/ophirc/mr_examples/wordcount/simple/out
